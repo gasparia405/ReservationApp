8f3328879b32c4b1e6286100780564eea41d690b
gasparia405 <gasparia405@gmail.com>
2018-05-26 13:57:55 -0400

    Initial commit

---
+++ resScript.js
@@ -0,0 +1,11 @@
+// capture input from form field
+// find the form and input
+const regForm = document.getElementById('registrar');
+const formInput = document.querySelector('input');
+
+// add event listener with anonymous function that looks at an event handler
+regForm.addEventListener('submit',
+    (e) => {
+
+    }
+);
